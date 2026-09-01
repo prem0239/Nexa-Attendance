8f828fdd0722309cacda7fcdd73266209be6e9f4
prem <prembhoi2006@gmail.com>
2026-09-01 22:12:09 +0530

    Finiishing the Project

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ from src.components.dialog_auto_enroll import auto_enroll_dialog
 
 
 def main():
+    st.set_page_config(
+        page_title = 'Nexa Attend - An AI Attendance System',
+        page_icon = 'https://raw.githubusercontent.com/prem0239/delta-demo/refs/heads/main/logo2.png'
+    )
 
     if 'login_type' not in st.session_state:
         st.session_state['login_type'] = None
